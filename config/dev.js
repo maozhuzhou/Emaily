a8dc450e0d5248a0c9836cfcc2e398b609ec9c09
maozhuzhou <867655330@qq.com>
2021-08-01 10:44:44 +0800

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+// dev.js - dev keys here!!
+module.exports = {
+    googleClientID: '1004277227864-a7fjbiuu4atf5v43q1psj88r09dl6lbg.apps.googleusercontent.com',
+    googleClientSecret: 'mDcaE2jJwJ6TIqYfE3ItWnVB',
+    mongoURI: 'mongodb+srv://lily:lily@cluster0.s2vjf.mongodb.net/emaily?retryWrites=true&w=majority',
+    mongoURI_PROD: 'mongodb+srv://lily:lily@cluster0.tdjrs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'jlkjfdklajfdajiejijijfdf',
+    HTTP_PROXY: 'http://127.0.0.1:50841',
+    stripePublishableKey: 'pk_test_51IhsfaAekLQRaY8qXOXc6j9NylN2o2XeSXP6GDeUCPn8zLpwWpiRyk2qUC81R4JrNx4dFbtTe7kWa0KDPhQv0KGO00fvLEEATT',
+    stripeSecretKey: 'sk_test_51IhsfaAekLQRaY8qSBZQPRG1YNg06AAnqnklcqN3aJnrgNK9JLqV93yjA5eddU6awe6JEUOcv5yQDeqM7S5Xbrqt00i2CjAcQA',
+    sendGridKey: '',
+    redirectDomain: 'http://localhost:3000',
+  };
+  
